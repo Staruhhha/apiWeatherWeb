fc4684efe0345fe2352cab43a5b713fd3fcf6246
Михаил Старухин <staruhhha@air-mihail-3.res.rea.ru>
2023-11-28 14:51:37 +0300

    first commit

---
+++ config.js
@@ -0,0 +1,8 @@
+const constants = {
+    openWeatherMap:{
+        BASE_URL: "https://api.openweathermap.org/data/2.5/weather?q=Moscow&units=metric",
+        SECRET_KEY: "e7c99e88a244ba72d0c61a1a807859f8"
+    }
+}
+
+module.exports = constants
